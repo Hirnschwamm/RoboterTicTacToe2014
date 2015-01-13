e4c370fec82b62571cbb46f41b5ff2c1529c48e2
92Garfield <92Garfield@here.de>
2015-01-13 13:18:47 +0100

    Cleanup GoTo

--- src/assignments/TicTacToe/GoTo.cpp
+++ src/assignments/TicTacToe/GoTo.cpp
@@ -29,240 +29,197 @@ bool GoTo::fire(ArActionDesired *myDesired)
     ArLog::log(ArLog::Normal, "I'm at: %ix%i; %i / %i; %ix%i", (int) curPos.getX(), (int) curPos.getY(), (int) curPos.getTh(), (int) targetHeading, (int) target.getX(), (int) target.getY());
 
 
-    //if (myRobot->isHeadingDone()) {
-        switch(state) {
-            case 0: {
-                myRobot->setVel(0);
-                //myDesired->setHeading(targetHeading);
+    switch(state) {
+        case 0: { //initial angle adjustment
+            myRobot->setVel(0);
+            int turnDir = 1;
+
+            int turnSpeed = (targetHeading - curPos.getTh());
+
+            if ((int)(myRobot->getTh() - targetHeading + 360) % 360>180) {
+                turnDir = 1;
+            } else {
+                turnDir = -1;
+            }
+            if (turnSpeed < 0) turnSpeed *= -1;
+            if (turnSpeed < 1) turnSpeed = 0;
+            if (turnSpeed > 5) turnSpeed = 5;
+
+            //myRobot->setRotVel(turnSpeed * turnDir);
+            myRobot->setHeading(targetHeading);
+
+            int deltaHeading = ((int)(curPos.getTh() - targetHeading) + 360) % 360;
+            if (deltaHeading > 180) deltaHeading = 360 - deltaHeading;
+            if (deltaHeading < 2) stateChange(1);
+            if (stateTime > 30) stateChange(2);
+            break;
+        }
+
+        case 1: { //check angle (unecessary / old code)
+            if (curPos.getTh() < targetHeading -1 || curPos.getTh() > targetHeading + 1) {
+                stateChange(-1);
+            } else {
+                stateChange(1);
+                initDirection = false;
+            }
+            break;
+        }
+
+        case 2: { //move to point
+            int deltaHeading = ((int)(curPos.getTh() - targetHeading) + 360) % 360;
+            if (deltaHeading > 180) deltaHeading = 360 - deltaHeading;
+            if (deltaHeading > 10) {
+                stateChange(-2);
+                //myRobot->setVel(0);
+            } else {
                 int turnDir = 1;
-                //if (curPos.getTh() > targetHeading) turnDir = -1;
 
                 int turnSpeed = (targetHeading - curPos.getTh());
-                //if (turnSpeed < 0) {
-                //    turnSpeed *= -1;
-                //    turnDir = 1;
-                //}
                 if ((int)(myRobot->getTh() - targetHeading + 360) % 360>180) {
                     turnDir = 1;
                 } else {
                     turnDir = -1;
                 }
                 if (turnSpeed < 0) turnSpeed *= -1;
-                if (turnSpeed < 1) turnSpeed = 0;
+                if (turnSpeed < 2) turnSpeed = 0;
                 if (turnSpeed > 5) turnSpeed = 5;
-                //if (turnSpeed < -5) turnSpeed = -5;
-                //if (turnSpeed > 0 && turnSpeed < 3) turnSpeed = 3;
-                //if (turnSpeed < 0 && turnSpeed > -3) turnSpeed = -3;
 
                 //myRobot->setRotVel(turnSpeed * turnDir);
                 myRobot->setHeading(targetHeading);
-
-                int deltaHeading = ((int)(curPos.getTh() - targetHeading) + 360) % 360;
-                if (deltaHeading > 180) deltaHeading = 360 - deltaHeading;
-                //if (curPos.getTh() > targetHeading - 1 && curPos.getTh() < targetHeading + 1) stateChange(1);
                 //myRobot->setDeltaHeading(turnDir * deltaHeading);
-                if (deltaHeading < 2) stateChange(1);
-                if (stateTime > 30) stateChange(2);
-                break;
-            }
 
-            case 1: {
-                if (curPos.getTh() < targetHeading -1 || curPos.getTh() > targetHeading + 1) {
-                    stateChange(-1);
-                } else {
+                int targetVel = minVel;
+                if (dist > maxVel * 2) {
+                    targetVel = maxVel;
+                } else if (dist > minVel * 2) {
+                    targetVel = dist / 2;
+                } else if (dist < 100) {
+                    targetVel = 0;
                     stateChange(1);
-                    initDirection = false;
                 }
-                break;
+                myRobot->setVel(targetVel);
             }
+            break;
+        }
 
-            case 2: {
-                int deltaHeading = ((int)(curPos.getTh() - targetHeading) + 360) % 360;
-                if (deltaHeading > 180) deltaHeading = 360 - deltaHeading;
-                if (deltaHeading > 10) {
-                    stateChange(-2);
-                    //myRobot->setVel(0);
+        case 3: { //if waypoints have angle, rotate to it
+            if (targetAngle) {
+                myRobot->setHeading((double) target.getTh());
+                 if(curPos.getTh() > target.getTh() - 2 && curPos.getTh() < target.getTh() + 2) stateChange(1);
+            } else {
+                stateChange(2);
+            }
+            break;
+        }
+
+        case 4: { //check angle (unecessary / old code)
+            if(curPos.getTh() < target.getTh() - 2 || curPos.getTh() > target.getTh() + 2) {
+                stateChange(-1);
+            } else {
+                stateChange(1);
+            }
+            break;
+        }
+
+        case 5: { //done / next waypoint
+            if (wpPos > path->size() - 2) {
+                stateChange(1);
+                ArLog::log(ArLog::Normal, "Done.");
+                return true;
+            } else {
+                ArLog::log(ArLog::Normal, "reached %i/%i (%i) %ix%i", wpPos, path->size(), curWp->id, curWp->x, curWp->y);
+                wpPos++;
+                if (wpPos == 1 || wpPos == path->size() - 1) {
+                    //next wp
+                    stateChange(-5);
                 } else {
-                    int turnDir = 1;
+                    //readjust own position
+                    stateChange(1);
+                }
+            }
+            break;
+        }
 
-                    int turnSpeed = (targetHeading - curPos.getTh());
-                    if ((int)(myRobot->getTh() - targetHeading + 360) % 360>180) {
-                        turnDir = 1;
+        case 6: { //adjust own position by laser readings
+            ArLaser *myLaser = action->getLaser();
+            //system("clear");
+
+            myLaser->lockDevice();
+            if (myLaser->isTryingToConnect()) {
+                printf("Laser trying to connect");
+            } else if (myLaser->isConnected()) {
+                printf("Has Laser %i, %i, %i\n", myRobot->hasLaser(myLaser), myLaser->isTryingToConnect(), myLaser->isConnected());
+                std::vector<ArSensorReading> *myReadings = myLaser->getRawReadingsAsVector();
+                for (int i = 0; i < myReadings->size(); i++) {
+                    if (i % 5 > 0) continue;
+                    ArSensorReading curReading = (*myReadings)[i];
+                    if (i < 10) printf(" ");
+                    if (i < 100) printf(" ");
+                    //printf("%i reading: %i\n", i, (int)curReading.getRange());
+                }
+
+                if (myReadings->size() > 180) {
+                    int rotvel = (*myReadings)[95].getRange() - (*myReadings)[85].getRange();
+                    if (rotvel < -10) rotvel = -10;
+                    if (rotvel > 10) rotvel = 10;
+                    if (rotvel * rotvel > 25) {
+                        myRobot->setRotVel(rotvel/2);
                     } else {
-                        turnDir = -1;
-                    }
-                    if (turnSpeed < 0) turnSpeed *= -1;
-                    if (turnSpeed < 2) turnSpeed = 0;
-                    if (turnSpeed < 1) turnSpeed = 0;
-                    if (turnSpeed > 5) turnSpeed = 5;
+                        ArPose rePose(0, 0, 0);
 
-                    //myRobot->setRotVel(turnSpeed * turnDir);
-                    myRobot->setHeading(targetHeading);
-                    //myRobot->setDeltaHeading(turnDir * deltaHeading);
+                        ArPose upRight = action->getMap()->getLineMaxPose();
+                        ArPose lowLeft = action->getMap()->getLineMinPose();
 
-                    int targetVel = minVel;
-                    if (dist > maxVel * 2) {
-                        targetVel = maxVel;
-                    } else if (dist > minVel * 2) {
-                        targetVel = dist / 2;
-                    } else if (dist < 100) {
-                        targetVel = 0;
-                        stateChange(1);
-                    }
-                    myRobot->setVel(targetVel);
-                }
-                break;
-            }
-
-            case 3: {
-                if (targetAngle) {
-                    myRobot->setHeading((double) target.getTh());
-                     if(curPos.getTh() > target.getTh() - 2 && curPos.getTh() < target.getTh() + 2) stateChange(1);
-                } else {
-                    stateChange(2);
-                }
-                break;
-            }
-
-            case 4: {
-                if(curPos.getTh() < target.getTh() - 2 || curPos.getTh() > target.getTh() + 2) {
-                    stateChange(-1);
-                } else {
-                    stateChange(1);
-                }
-                break;
-            }
-
-            case 5: {
-                if (wpPos > path->size() - 2) {
-                    stateChange(1);
-                    ArLog::log(ArLog::Normal, "Done.");
-                    return true;
-                } else {
-                    ArLog::log(ArLog::Normal, "reached %i/%i (%i) %ix%i", wpPos, path->size(), curWp->id, curWp->x, curWp->y);
-                    wpPos++;
-                    if (wpPos == 1 || wpPos == path->size() - 1) {
-                        //next wp
-                        stateChange(-5);
-                    } else {
-                        //readjust own position
-                        stateChange(1);
-                    }
-                }
-                break;
-            }
-
-            case 6: {
-                ArLaser *myLaser = action->getLaser();
-                //system("clear");
-
-                myLaser->lockDevice();
-                if (myLaser->isTryingToConnect()) {
-                    printf("Laser trying to connect");
-                } else if (myLaser->isConnected()) {
-                    printf("Has Laser %i, %i, %i\n", myRobot->hasLaser(myLaser), myLaser->isTryingToConnect(), myLaser->isConnected());
-                    std::vector<ArSensorReading> *myReadings = myLaser->getRawReadingsAsVector();
-                    for (int i = 0; i < myReadings->size(); i++) {
-                        if (i % 5 > 0) continue;
-                        ArSensorReading curReading = (*myReadings)[i];
-                        if (i < 10) printf(" ");
-                        if (i < 100) printf(" ");
-                        //printf("%i reading: %i\n", i, (int)curReading.getRange());
-                    }
-
-                    if (myReadings->size() > 180) {
-                        int rotvel = (*myReadings)[95].getRange() - (*myReadings)[85].getRange();
-                        if (rotvel < -10) rotvel = -10;
-                        if (rotvel > 10) rotvel = 10;
-                        if (rotvel * rotvel > 25) {
-                            myRobot->setRotVel(rotvel/2);
-                        } else {
-                            ArPose rePose(0, 0, 0);
-
-                            ArPose upRight = action->getMap()->getLineMaxPose();
-                            ArPose lowLeft = action->getMap()->getLineMinPose();
-
-                            myRobot->setRotVel(0);
-                            int angle = round(myRobot->getPose().getTh() / 90) * 90;
-                            rePose.setTh(angle);
-                            switch (angle) {
-                            case 0: {
-                                printf("0>");
-                                rePose.setY((int)(*myReadings)[180].getRange() + lowLeft.getY());
-                                rePose.setX(-(int)(*myReadings)[90].getRange() + upRight.getX());
-                                break;
-                            }
-
-                            case 90: {
-                                printf("90>");
-                                rePose.setY(-(int)(*myReadings)[90].getRange() + upRight.getY());
-                                rePose.setX(-(int)(*myReadings)[180].getRange() + upRight.getX());
-                                break;
-                            }
-
-                            case 180: {
-                                printf("180>");
-                                rePose.setY(-(int)(*myReadings)[180].getRange() + upRight.getY());
-                                rePose.setX((int)(*myReadings)[90].getRange() + lowLeft.getX());
-                                break;
-                            }
-
-                            case 270: {
-                                printf("270>");
-                                rePose.setY((int)(*myReadings)[90].getRange() + lowLeft.getY());
-                                rePose.setX((int)(*myReadings)[180].getRange() + lowLeft.getX());
-                                break;
-                            }
-
-                            }
-                            printf("My Pose: %ix%i / %i; %i\n", (int)rePose.getX(), (int)rePose.getY(), (int)rePose.getTh(), angle);
-                            myRobot->moveTo(rePose);
-                            stateChange(-6);
+                        myRobot->setRotVel(0);
+                        int angle = round(myRobot->getPose().getTh() / 90) * 90;
+                        rePose.setTh(angle);
+                        switch (angle) {
+                        case 0: {
+                            printf("0>");
+                            rePose.setY((int)(*myReadings)[180].getRange() + lowLeft.getY());
+                            rePose.setX(-(int)(*myReadings)[90].getRange() + upRight.getX());
+                            break;
                         }
 
+                        case 90: {
+                            printf("90>");
+                            rePose.setY(-(int)(*myReadings)[90].getRange() + upRight.getY());
+                            rePose.setX(-(int)(*myReadings)[180].getRange() + upRight.getX());
+                            break;
+                        }
+
+                        case -180:
+                        case 180: {
+                            printf("180>");
+                            rePose.setY(-(int)(*myReadings)[180].getRange() + upRight.getY());
+                            rePose.setX((int)(*myReadings)[90].getRange() + lowLeft.getX());
+                            break;
+                        }
+
+                        case -90: {
+                            printf("-90>");
+                            rePose.setY((int)(*myReadings)[90].getRange() + lowLeft.getY());
+                            rePose.setX((int)(*myReadings)[180].getRange() + lowLeft.getX());
+                            break;
+                        }
+
+                        }
+                        printf("My Pose: %ix%i / %i; %i\n", (int)rePose.getX(), (int)rePose.getY(), (int)rePose.getTh(), angle);
+                        myRobot->moveTo(rePose);
+                        stateChange(-6);
                     }
 
-                } else {
-                    myLaser->asyncConnect();
                 }
 
-                myLaser->unlockDevice();
-                break;
-            }
-        }
-        stateTime++;
-    //}
-
-    /*
-    if (myRobot->isHeadingDone()) {
-        if (curPos.getTh() < targetHeading - 1 || curPos.getTh() > targetHeading + 1) {
-            myDesired->setHeading(targetHeading);
-        } else if (dist > 10) {
-            state = 1;
-            int targetVel = minVel;
-            if (dist > maxVel * 2) {
-                targetVel = maxVel;
-            } else if (dist > minVel) {
-                targetVel = dist;
-            }
-            myDesired->setVel(targetVel);
-        } else if(targetAngle && (curPos.getTh() < target.getTh() - 2 || curPos.getTh() > target.getTh() + 2)) {
-            state = 2;
-            myDesired->setHeading((double) target.getTh());
-        }
-
-        if (dist < 10) {
-            if (path->size() > wpPos + 1) {
-                wpPos++;
             } else {
-                myDesired->setVel(0);
-                printf("Done.");
+                myLaser->asyncConnect();
             }
+
+            myLaser->unlockDevice();
+            break;
         }
-
     }
-    */
-
+    stateTime++;
 
     return false;
 }
